bd16778dccb4b206711d2817f3e76a585680a1c3
Gabriel Witte <119006614+gabrielbwitte@users.noreply.github.com>
2024-11-06 13:15:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use("/", publicRoutes);
 app.use("/", auth, privateRoutes);
 
-const port = 1000;
+const port = 4000;
 
 app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
